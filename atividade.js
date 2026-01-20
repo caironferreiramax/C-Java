17f3fe43bb7e27eef47cbf98c1abb8c18f40c62b
caironferreiramax <caironferreira01@gmail.com>
2026-01-20 20:59:41 -0300

    Atividade Java

--- atividade.js
+++ atividade.js
@@ -16,15 +16,99 @@
 
 // console.log("Conversão: " + (real / dolar));
 
+// 3 Novo- let real = 400.0;
+// let dolar = 5.0;
+// let resultado = real / dolar;
+
+// console.log(resultado)
+
 // 4- let num = 5;
 // let cubo = num ** 3;
 
 // console.log(cubo)
 
-// let num1 = 6
+// let val = 5;
+// let cubo = val ** 3
+
+// console.log(cubo)
+
+// 5 - let num1 = 6
 // let num2 = 7
 // let num3 = 8
 // let num4 = 4
 
-// console.log("Média é:" + ((num1+num2+num3+num4) /4))
+// 6- console.log("Média é:" + ((num1+num2+num3+num4) /4))
+
+// let celsius = 32.5
+// let formula = (celsius * 1.8)
+// let resultado = formula + 32
+
+// console.log("Conversão do celsius para F é:" + (resultado))
+
+// let C = 32.5
+// let F = (C * 9) / 5 + 32
+
+// console.log("Conversão do celsius para F é:" + (F))
+
+// 7- let resultado = 15 % 4;
+// console.log(resultado);
+
+// let desconto = 0.15;
+// let produto = 80;
+// let valordesconto = produto * desconto;
+// let valorfinal = produto - valordesconto;
+
+// console.log("Desconto R$:" + (valordesconto));
+// console.log("Valor Final:" + (valorfinal));
+
+// let numero = 20;
+// let sucessor = +1;
+// let antecessor = -1;
+
+// console.log("O número sucessor é: " + (numero + sucessor));
+// console.log("O número antecessor é: " + (numero + antecessor));
+
+// let peso = 80.0;
+// let altura = 1.80;
+// let imc = peso / (altura ** 2);
+
+// console.log("O IMC é: " + (imc));
+
+// let numero = 10;
+// let texto = "10";
+
+// let comparacao = (numero === texto);
+
+// console.log(comparacao);
+
+// let A = 16;
+// let B = 18;
+
+// let comparacaoID = (A >= B);
+
+// console.log(comparacaoID);
+
+// let texto1 = "Javascript";
+// let texto2 = "javascript";
+
+// let comparacaotexto = (texto1 !== texto2);
+
+// console.log(comparacaotexto);
+
+// let num1 = 6;
+// let num2 = 0;
+
+// let comparacaonum = (num1 <= num2);
+
+// console.log(comparacaonum);
+
+let texto1 = barcelona;
+
+
+console.log(resultado);
+
+
+
+
+
 
