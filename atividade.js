7b1ea2aa573eba9a45715e4ea9c9b9f484e1bb9e
caironferreiramax <caironferreira01@gmail.com>
2026-01-19 21:03:23 -0300

    Atividade Java

---
+++ atividade.js
@@ -0,0 +1,30 @@
+// 1- let num1 = 2;
+// let num2 = 10;
+
+// console.log("Soma: " + (num1+num2));
+// console.log("Subtração: " + (num1 - num2));
+// console.log("Multiplicação: " + (num1 * num2));
+// console.log("Divisão: " + (num1 / num2));
+
+// 2- let base = 10;
+// let altura = 4;
+
+// console.log("Área do retângulo: " + (base * altura));
+
+// 3- let real = 400;
+// let dolar = 5.0;
+
+// console.log("Conversão: " + (real / dolar));
+
+// 4- let num = 5;
+// let cubo = num ** 3;
+
+// console.log(cubo)
+
+// let num1 = 6
+// let num2 = 7
+// let num3 = 8
+// let num4 = 4
+
+// console.log("Média é:" + ((num1+num2+num3+num4) /4))
+
