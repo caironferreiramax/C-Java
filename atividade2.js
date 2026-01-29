02d0207b8553e6d8f23d749d3ec6e2ecda346157
caironferreiramax <caironferreira01@gmail.com>
2026-01-29 14:00:51 -0300

    Atividade Java

--- atividade2.js
+++ atividade2.js
@@ -1,30 +1,31 @@
 // let dia = 3
 
 // switch (dia) {
-//     case 1:{
+
+//     case 1: {
 //         console.log("1 é Domingo");
 //         break
 //     } 
-//     case 2:{
+//     case 2: { 
 //         console.log("2 é Segunda");
 //         break;    
 //     }
-//     case 3:{
+//     case 3: {
 //         console.log("3 é Terça");
 //     } break;
-//     case 4:{
+//     case 4: {
 //         console.log("4 é Quarta");
 //         break;
 //     }
-//     case 5:{
+//     case 5: {
 //         console.log("5 é Quinta");
 //         break;
 //     }
-//     case 6:{
+//     case 6: {
 //         console.log("6 é Sexta");
 //         break;
 //     }
-//     case 7:{
+//     case 7: {
 //         console.log("7 é Sábado");
 //         break;
 //     }
@@ -32,25 +33,97 @@
 //         console.log("Não é um dia da semana");
 //         break;
 //     }
-// }
-    
-let mes = fevereiro
 
-switch (mes) {
-    case janeiro: {
-        console.log("Verão")
-        break;
-    }
-    case fevereiro: {
-        console.log("Verão")
-        break;
-    }
-    case março: {
-        console.log("Verão")
-        break;
-    }
-    case abril: {
-        console.log("Outono")
-        break;
-    }
-}
+// }
+
+// let mes = "Junho";
+
+// switch (mes) {
+    
+//   case "Janeiro": 
+//   case "Fevereiro":
+//   case "Março":
+//     console.log("Verão");
+//     break;
+//   case "Abril":
+//   case "Maio":
+//   case "Junho": 
+//     console.log("Outono"); 
+//     break; 
+//   case "Julho":
+//   case "Agosto":
+//   case "Setembro":
+//     console.log("Inverno");
+//     break;
+//   case "Outubro":
+//   case "Novembro":
+//   case "Dezembro":
+//     console.log("Primavera");
+//     break;
+//   default:
+//     console.log("Mês inválido");
+//     break
+// }
+
+// let num = 10;
+// let num1 = 5;
+// let operador = ",";
+// let resultado;
+
+// switch (operador) { 
+//     case "+": { 
+//         resultado = num + num1;
+//         console.log("Adição: " + resultado);
+//         break;
+//     }
+//     case "-": { 
+//         resultado = num - num1;
+//         console.log("Subtração: " + resultado);
+//         break;
+//     }
+//     case "*": { 
+//         resultado = num * num1;
+//         console.log("Multiplicação: " + resultado);
+//         break;
+//     }
+//     case "/": { 
+//         resultado = num / num1;
+//         console.log("Divisão: " + resultado);
+//         break;
+//     }
+//     default: { 
+//         console.log("Operador Inválido")
+//         break
+//     }
+// }
+
+// console.log(resultado);
+
+// let nota = "B";
+
+// switch (nota) {
+//     case "A": {
+//         console.log("Excelente");
+//         break;
+//     }
+//     case "B": { 
+//         console.log("Bom");
+//         break;
+//     }
+//     case "C": { 
+//         console.log("Regular");
+//         break;
+//     }
+//     case "D": {
+//         console.log("Ruim");
+//         break;
+//     }
+//     case "F": { 
+//         console.log("Reprovado");
+//         break;
+//     }
+//     default: {
+//         console.log("Nota desconhecida")
+//         break
+//     }
+// } 
