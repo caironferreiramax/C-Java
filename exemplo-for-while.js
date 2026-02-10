b4f039aa18df1efa85f1f1f81bc607be68e93f11
caironferreiramax <caironferreira01@gmail.com>
2026-02-10 20:59:28 -0300

    Atividade Java

---
+++ exemplo-for-while.js
@@ -0,0 +1,109 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(`Valor de i: ${i}`);
+// }
+
+// let i;
+// for (i = 0; i < 5; i++) {
+    
+//     if(i == 3 || i == 4) {
+//         continue;
+//     }
+//     console.log(`Valor de i: ${i}`);
+// }
+//     console.log(`Valor de i: ${i}`);
+
+//  let i;
+//  for (i = 0; i < 5; i++) {
+//     console.log(`Valor de i: ${i}`);
+// }
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i <= 15; i +=2) {
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     let resultado = 7 * i;
+//     console.log("7x" + i + " = " + resultado)
+// }
+
+// let num = 7
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(num * i);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i+i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let op = 0
+// while(op == 0){
+//       console.log(`Hello World`);
+//       op = 2;
+// }
+
+// do {
+//     console.log(`Hello World2`); 
+// } while (op == 0);
+
+// let oi = 10
+// while(oi > 0) {
+//     console.log("maçã");
+//     oi--;
+// }
+
+
+// let oi = 10
+// do {
+
+//     let j = 99;
+//     j += 235;
+//     if(j >= 300) {
+//         console.log("J é:" + j);
+//     }
+//     oi--;
+// }   while (oi > 0);
+
+
