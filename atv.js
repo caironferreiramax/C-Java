93ed273be727d97bfe57b8f0eb646042f8235e78
caironferreiramax <caironferreira01@gmail.com>
2026-02-26 20:50:07 -0300

    Atividade Java

---
+++ atv.js
@@ -0,0 +1,56 @@
+// let num = parseInt(prompt("Digite um número:"));
+// console.log(num)
+// while(num < 100 || isNaN(num)){
+    //     alert("Número Inválido!");
+    //     num = parseInt(prompt("Digite um número:"));
+    // }
+    
+    // alert("Parábens!!")
+    
+    //A senha do usuário deve ser "123", faça até ele acertar.
+    
+    // let senha = parseInt(prompt("Digite a senha"));
+    // while(senha !== 123){
+    //     alert("Senha Incorreta!");
+    //     senha = parseInt(prompt("Digite a senha"));
+    // }
+    // alert("Login Autorizado!")
+
+
+
+    // let time = 10;
+// let start = confirm("Deseja iniciar o cronômetro?");
+//     while(start && time > 0){
+//     alert(`tempo: ${time}`);
+//     --time;
+
+//     if(time == 0){
+//         alert("Lançar!");
+//     }
+
+// }
+
+// Crie um menu 1café, 2chá, 3sair.
+
+
+
+// let continuar = true;
+//     while(continuar) {
+//     let C = parseFloat(prompt("Informe uma temperatura em °C: "));
+//     let F = (C*9) / 5 + 32;
+//     alert("A temperatura em fahrenhet é: " + F);
+//     continuar = confirm("Deseja inserir uma nova temperatura?");
+// }
+
+
+
+
+// let JavaScript = "Javascript";
+// let javaScript = "javascript";
+
+// if(JavaScript == javaScript){
+//     console.log("Sim, as string são iguais");
+// } else {
+//     console.log("Não, a string são diferentes");
+// }
+
