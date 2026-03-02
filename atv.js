6db19f8f5f3588f7edc82ece26563b4e9f484aa6
caironferreiramax <caironferreira01@gmail.com>
2026-03-02 20:55:38 -0300

    Atividade Java

--- atv.js
+++ atv.js
@@ -18,7 +18,7 @@
 
 
 
-    // let time = 10;
+// let time = 10;
 // let start = confirm("Deseja iniciar o cronômetro?");
 //     while(start && time > 0){
 //     alert(`tempo: ${time}`);
@@ -30,27 +30,58 @@
 
 // }
 
-// Crie um menu 1café, 2chá, 3sair.
+// Crie um menu 
+
+// let menu = prompt("Olá, qual o seu pedido?");
+// while(menu !== "café"){
+//     alert("Item fora do menu");
+//     menu = prompt("Olá, qual o seu pedido?");
+// } alert("Pedido feito!")
+
+
+// Saiba o seu IMC
+// let peso = 80.0;
+// let altura = 1.80;
+// let imc = peso / (altura ** 2);
+
+// let peso = parseFloat(prompt("Digite o seu peso"));
+// let altura = parseFloat(prompt("Digite a sua altura"));
+//     let imc = peso / (altura * altura);
+//     alert("O seu imc é: " + imc;
+
+// console.log("O IMC dessa pessoa é: " + imc);
+
+    // let peso = parseFloat(prompt("Digite o seu peso"));
+    // let altura = parseFloat(prompt("Digite a sua altura"));
+    
+    // if (isNaN(peso) || isNaN(altura)) {
+    //     alert("Digite apenas números. Use (.) para números decimais!!")
+    // }
+    
+    // else{
+    //     let imc = peso / (altura * altura);
+
+    //     alert("O seu imc é: " + imc.toFixed(2));
+    // }
+
+// Calcule a área de um retângulo (base x altura) e mostre o resultado
+
+// let base = parseFloat(prompt("Digite o valor da base"));
+// let altura = parseFloat(prompt("Digite o valor da altura"));
+// let retangulo = base * altura;
+// alert("O valor da área é: " + retangulo);
+
+// let nota1 = parseFloat(prompt("Digite a primeira nota"));
+
 
 
 
 // let continuar = true;
+
 //     while(continuar) {
 //     let C = parseFloat(prompt("Informe uma temperatura em °C: "));
 //     let F = (C*9) / 5 + 32;
 //     alert("A temperatura em fahrenhet é: " + F);
 //     continuar = confirm("Deseja inserir uma nova temperatura?");
-// }
-
-
-
-
-// let JavaScript = "Javascript";
-// let javaScript = "javascript";
-
-// if(JavaScript == javaScript){
-//     console.log("Sim, as string são iguais");
-// } else {
-//     console.log("Não, a string são diferentes");
 // }
 
